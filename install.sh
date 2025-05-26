d125047108c1c6a8d67b59a4edeb998ed8cc2089
dnagy-ctrl <nagydavidmanuel@gmail.com>
2025-05-26 22:14:26 +0100

    upload from m2sata before moving to ssd on thinkpad t440p

--- install.sh
+++ install.sh
@@ -1,4 +1,14 @@
-ln -s ~/.dotfiles/waybar ~/.config/waybar
-ln -s ~/.dotfiles/yay ~/.config/yay
+rm -rf ~/.config/ghostty
 ln -s ~/.dotfiles/ghostty ~/.config/ghostty
+rm -rf ~/.config/hypr/
 ln -s ~/.dotfiles/hypr ~/.config/hypr
+rm -rf ~/.config/neofetch/
+ln -s ~/.dotfiles/neofetch/
+rm -rf ~/.config/nvim/
+ln -s ~/.dotfiles/nvim/ ~/.config/nvim/
+rm -rf ~/.config/waybar/
+ln -s ~/.dotfiles/waybar ~/.config/waybar
+rm -rf ~/.zshrc
+ln -s ~/.dotfiles/.zshrc ~/
+rm -rf /etc/pacman.conf 
+ln -s ~/.dotfiles/pacman.conf /etc/
